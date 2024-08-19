c2b05071d005a457e5532defda4e6f14e89b2daa
collinsln <collangat@gmail.com>
2024-08-19 12:42:48 +0300

    changes on home page

--- src/Home.js
+++ src/Home.js
@@ -8,15 +8,30 @@ function Home() {
         <h1>Collins Kipngeno</h1>
         <h2>Results-driven Software Engineer</h2>
         <p>
-          Experienced in Java, Angular, React, SQL, and C#. Specialized in biometrics and integrations, with a BSc in Analytical Chemistry with Computing.
+        I am a seasoned Software Engineer with a solid 
+        background in both front-end and back-end development, 
+        specializing in Java, Angular, React, SQL, and C#. 
+        With a BSc in Analytical Chemistry with Computing, 
+        I have honed my skills in biometrics, integrations, 
+        and algorithmic problem-solving. My career includes
+         successful projects in solar energy and data engineering, 
+         where I designed scalable solutions, optimized software 
+         performance, and led teams to deliver impactful results.
         </p>
         
       </div>
       <div className="summary">
         <h3>About Me</h3>
         <p>
-          I'm a full-stack developer with a passion for algorithmic problem-solving and designing effective solutions. 
-          I thrive in collaborative environments and am dedicated to delivering high-quality software solutions.
+        As a full-stack developer passionate about
+         creating effective software solutions, 
+         I excel in collaborative environments and am 
+         dedicated to enhancing user experiences through 
+         innovative technologies. My expertise spans across
+          multiple domains, including cloud computing,
+           API development, and database management, reflecting
+            my commitment to both technical excellence and 
+            continuous learning.
         </p>
       </div>
     </div>
